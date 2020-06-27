c03f9b25a6d39bfc3bc684be39ac356be758266d
William Collins <bcollins@Williams-MacBook-Pro.local>
2020-06-26 21:08:48 -0400

    set up canvas

---
+++ BlazorGalaga/Models/Word.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace BlazorAnimate.Models
+{
+    public class Word
+    {
+        public string TextString { get; set; }
+        public PointF Location { get; set; }
+        public BezierCurve Path { get; set; }
+        public bool DrawPath { get; set; }
+    }
+}
---
+++ BlazorGalaga/Services/WordService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Blazor.Extensions.Canvas.Canvas2D;
+using BlazorAnimate.Models;
+
+namespace BlazorAnimate.Services
+{
+    public class WordService
+    {
+        public async void DrawText(Canvas2DContext ctx, Word word)
+        {
+            await ctx.SetFillStyleAsync("#ffffff");
+            await ctx.SetFontAsync("70px Arial");
+            await ctx.FillTextAsync(word.TextString, word.Location.X, word.Location.Y);
+        }
+    }
+}
